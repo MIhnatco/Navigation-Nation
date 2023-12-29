5e8e2bccfdf31be48a8f59eb216ca3c2312beb3c
Ihliana <Ihnatco@protonmail.com>
2023-12-29 10:58:48 +0100

    update index.html
    intro to nav's animation

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+
+function toggleNav() {
+  //toggle: menu bars open/closed
+  menuBars.classList.toggle("change");
+
+  //toggle: Menu active
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    //Animate In-overlay
+    overlay.classList.remove("overlay-slide-left");
+    overlay.classList.add("overlay-slide-right");
+  } else {
+    //Animate Out - Overlay
+    overlay.classList.add("overlay-slide-left");
+    overlay.classList.remove("overlay-slide-right");
+  }
 }
+
+//Event Listeners
+menuBars.addEventListener("click", toggleNav);
+nav1.addEventListener("click", toggleNav);
+nav2.addEventListener("click", toggleNav);
+nav3.addEventListener("click", toggleNav);
+nav4.addEventListener("click", toggleNav);
+nav5.addEventListener("click", toggleNav);
